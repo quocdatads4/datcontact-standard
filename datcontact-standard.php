e8c0cf212f7f2d7ed3a9cb95a0173459a2491632
quocdatads4 <quocdatads4@gmail.com>
2025-05-16 19:19:16 +0700

    datcontact-standard-v2.0
    datcontact-standard-v2.0

--- datcontact-standard.php
+++ datcontact-standard.php
@@ -37,34 +37,58 @@ function datcontact_standard_register_settings() {
 function datcontact_standard_settings_page() {
 ?>
     <div class="wrap">
-        <h1>DATContact Standard Settings</h1>
-        <form method="post" action="options.php">
-            <?php settings_fields('datcontact-standard-settings-group'); ?>
-            <?php do_settings_sections('datcontact-standard-settings-group'); ?>
-            <table class="form-table">
-                <tr valign="top">
-                    <th scope="row">Link đặt hàng</th>
-                    <td><input type="text" name="link_order_standard" value="<?php echo esc_attr(get_option('link_order_standard')); ?>" /></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Link chat Zalo</th>
-                    <td><input type="text" name="link_zalo_standard" value="<?php echo esc_attr(get_option('link_zalo_standard')); ?>" /></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Gọi điện</th>
-                    <td><input type="text" name="link_phone_standard" value="<?php echo esc_attr(get_option('link_phone_standard')); ?>" /></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Link Messenger</th>
-                    <td><input type="text" name="link_messenger_standard" value="<?php echo esc_attr(get_option('link_messenger_standard')); ?>" /></td>
-                </tr>
-                <tr valign="top">
-                    <th scope="row">Nhắn tin SMS</th>
-                    <td><input type="text" name="link_sms_standard" value="<?php echo esc_attr(get_option('link_sms_standard')); ?>" /></td>
-                </tr>
-            </table>
-            <?php submit_button(); ?>
-        </form>
+        <div class="fluent-mail-main-menu-items"><ul role="menubar" class="fluent-mail-navigation el-menu--horizontal el-menu"><li role="menuitem" tabindex="0" class="el-menu-item" style="border-bottom-color: transparent;"><p style="font-size: 18pt;"><a href="https://datmarketing.edu.vn" style="text-decoration: none;"><strong><span style="color: #000000;">DAT</span><span style="color: #ff0000;">Marketing</span></strong><span style="color: #000000;">™</span></a></p></li> <li role="menuitem" tabindex="0" class="el-menu-item is-active" style="">Cài đặt</li></ul></div>
+        <!-- New structure based on fluent-mail-admin.css -->
+        <div class="fluent-mail-body">
+            <div class="el-row" style="margin-left: -10px; margin-right: -10px;">
+                <div class="el-col el-col-24 el-col-sm-24 el-col-md-10" style="padding-left: 10px; padding-right: 10px;">
+                    <div class="fss_content_box fss_box_action fss_box_active" style="margin-bottom: 0px;">
+                        <div class="fss_header">
+                            Cài đặt Chung
+                        </div>
+                        <div class="fss_content">
+                            <div class="fss_general_settings">
+                                <form method="post" action="options.php" class="el-form fss_compact_form el-form--label-top">
+                                    <?php settings_fields('datcontact-standard-settings-group'); ?>
+                                    <?php do_settings_sections('datcontact-standard-settings-group'); ?>
+<div class="el-form-item">
+                                        <label class="el-form-item__label">Link đặt hàng</label>
+                                        <div class="el-form-item__content">
+                                            <input type="text" name="link_order_standard" value="<?php echo esc_attr(get_option('link_order_standard')); ?>" placeholder="Ví dụ: https://example.com/order" />
+                                        </div>
+                                    </div>
+                                    <div class="el-form-item">
+                                        <label class="el-form-item__label">Số điện thoại Zalo</label>
+                                        <div class="el-form-item__content">
+                                            <input type="text" name="link_zalo_standard" value="<?php echo esc_attr(str_replace('https://zalo.me/', '', get_option('link_zalo_standard'))); ?>" placeholder="Ví dụ: 0569171809" />
+                                        </div>
+                                    </div>
+                                    <div class="el-form-item">
+                                        <label class="el-form-item__label">Số điện thoại</label>
+                                        <div class="el-form-item__content">
+                                            <input type="text" name="link_phone_standard" value="<?php echo esc_attr(str_replace('tel:', '', get_option('link_phone_standard'))); ?>" placeholder="Ví dụ: 0569171809" />
+                                        </div>
+                                    </div>
+                                    <div class="el-form-item">
+                                        <label class="el-form-item__label">Tên Messenger</label>
+                                        <div class="el-form-item__content">
+<input type="text" name="link_messenger_standard" value="<?php echo esc_attr(str_replace('https://m.me/', '', get_option('link_messenger_standard'))); ?>" placeholder="Ví dụ: tên-người-dùng" />
+                                        </div>
+                                    </div>
+                                    <div class="el-form-item">
+                                        <label class="el-form-item__label">Số điện thoại SMS</label>
+                                        <div class="el-form-item__content">
+                                            <input type="text" name="link_sms_standard" value="<?php echo esc_attr(str_replace('sms:', '', get_option('link_sms_standard'))); ?>" placeholder="Ví dụ: 0569171809" />
+                                        </div>
+                                    </div>
+                                    <?php submit_button(); ?>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 <?php
 }
@@ -75,19 +99,32 @@ function datcontact_standard_enqueue_frontend_styles() {
         'datcontact-standard-style',
         plugin_dir_url(__FILE__) . 'css/datcontact-standard-styles.css',
         array(),
-        '1.0' // Version number
+        '1.0'
     );
 }
 add_action('wp_enqueue_scripts', 'datcontact_standard_enqueue_frontend_styles');
 
 // Enqueue admin styles
-function datcontact_standard_enqueue_admin_styles() {
-    wp_enqueue_style(
-        'datcontact-standard-admin-style',
-        plugin_dir_url(__FILE__) . 'admin/datcontact-admin-styles.css',
-        array(),
-        '1.0' // Version number
-    );
+function datcontact_standard_enqueue_admin_styles($hook) {
+    // Load only on the plugin settings page
+    if (strpos($hook, 'datcontact-standard') === false) {
+        return;
+    }
+    
+    // Get the correct path to the CSS file
+    $css_path = plugin_dir_url(__FILE__) . 'admin/datcontact-admin-styles.css';
+    
+    // Verify the CSS file exists
+    if (file_exists(plugin_dir_path(__FILE__) . 'admin/datcontact-admin-styles.css')) {
+        wp_enqueue_style(
+            'datcontact-standard-admin-style',
+            $css_path,
+            array(),
+            filemtime(plugin_dir_path(__FILE__) . 'admin/datcontact-admin-styles.css') // Use filemtime for cache busting
+        );
+    } else {
+        error_log('DATContact Standard: CSS file not found at ' . $css_path);
+    }
 }
 add_action('admin_enqueue_scripts', 'datcontact_standard_enqueue_admin_styles');
 
@@ -96,10 +133,16 @@ add_action('wp_footer', 'datcontact_standard_button_contact_bar');
 
 function datcontact_standard_button_contact_bar() {
     $link_order = esc_url(get_option('link_order_standard'));
-    $link_zalo = esc_url(get_option('link_zalo_standard'));
-    $link_phone = esc_url(get_option('link_phone_standard'));
-    $link_messenger = esc_url(get_option('link_messenger_standard'));
-    $link_sms = esc_url(get_option('link_sms_standard'));
+    $zalo_number = get_option('link_zalo_standard');
+    $link_zalo = esc_url('https://zalo.me/' . $zalo_number);
+    $phone_number = get_option('link_phone_standard');
+    $link_phone = esc_url('tel:' . $phone_number);
+    
+    $messenger_name = get_option('link_messenger_standard');
+    $link_messenger = esc_url('https://m.me/' . $messenger_name);
+    
+    $sms_number = get_option('link_sms_standard');
+    $link_sms = esc_url('sms:' . $sms_number);
     $plugin_url = plugin_dir_url(__FILE__) . 'assets/icons/';
 ?>
     <div class="giuseart-nav">
@@ -111,13 +154,14 @@ function datcontact_standard_button_contact_bar() {
                     <span class="phone_animation animation-shadow">
                         <i class="icon-phone-w" aria-hidden="true"></i>
                     </span>
-                    <span class="btn_phone_txt">Gọi điện</span>
+<span class="btn_phone_txt">Gọi điện</span>
                 </a>
             </li>
             <li><a href="<?php echo $link_messenger; ?>" rel="nofollow" target="_blank"><i class="ticon-messenger"></i>Messenger</a></li>
             <li><a href="<?php echo $link_sms; ?>" class="chat_animation">
             <i class="ticon-chat-sms" aria-hidden="true" title="Nhắn tin sms"></i>Nhắn tin SMS</a></li>
             <li class="to-top-pc">
+<a href="#" rel="nofollow">
                 <a href="#" rel="nofollow">
                     <i class="ticon-angle-up" aria-hidden="true" title="Quay lên trên"></i>
                 </a>
